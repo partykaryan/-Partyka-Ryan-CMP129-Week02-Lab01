8cc3b3f994ed9fe3a2fca3f9b718ec62f99e85ee
partyka.ryan <partyka.ryan@student.ccm.edu>
2026-09-15 16:23:41 -0400

    Completed the Kinetic Energy Assignment for Lab 3

--- KineticEnergy.java
+++ KineticEnergy.java
@@ -6,14 +6,29 @@ public class KineticEnergy {
 
     Scanner keyboard = new Scanner(System.in);
 
-        
-    
+    //Variable Declaration
+    double mass;
+    double velocity; 
+    double energy;
+
     System.out.print("Enter the mass ==> ");
+    mass = keyboard.nextDouble();
 
     System.out.print("Enter the velocity ==>");
+    velocity = keyboard.nextDouble();
 
+    energy = kineticEnergy(mass, velocity);
 
+    System.out.println("The Kinetic Energy of an Object of mass " + mass + " kilograms" +
+    "\nand Velocity of " + velocity + " meters per second is " + energy);
 
+keyboard.close();
+}//end of main method
+
+//return method for calculating kinetic energy
+public static double kineticEnergy (double mass, double velocity)
+{
+    return 0.5 * mass * Math.pow(velocity, 2);
 }
 
-}
+}//end of public class
