f405f36b62d37be9fa83c633ef5d3e676d176da4
partyka.ryan <partyka.ryan@student.ccm.edu>
2026-09-15 13:51:20 -0400

    Working on Week 2 Lab 1

--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class KineticEnergy {
+
+    public static void main(String []args){
+
+    Scanner keyboard = new Scanner(System.in);
+
+        
+    
+    System.out.print("Enter the mass ==> ");
+
+    System.out.print("Enter the velocity ==>");
+
+
+
+}
 
 }
